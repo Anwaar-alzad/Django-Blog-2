51d653c22d159840f7143a66a87ee7af08634853
Anwaar-alzad <158757969+Anwaar-alzad@users.noreply.github.com>
2025-01-21 01:00:21 +0400

    post model created with 4 fields ( title, content, image, published_date )

---
+++ blog/blogapp/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length = 150)
+    content = models.CharField(max_length = 250)
+    image = models.ImageField(upload_to="images/")
+    published_date = models.DateTimeField(auto_now=False, auto_now_add=False)
+    
+    
+    
+    
+    def __str__(self):
+        return self.title
